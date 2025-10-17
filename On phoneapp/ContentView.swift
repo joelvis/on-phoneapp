f607e5729725432f335f7a4b5795f18c63c0ecce
joelvis <sdhinc@gmail.com>
2025-10-17 12:56:05 -0700

    Initial Commit

---
+++ On phoneapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  On phoneapp
+//
+//  Created by Joel  on 10/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ On phoneapp/On_phoneappApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_phoneappApp.swift
+//  On phoneapp
+//
+//  Created by Joel  on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct On_phoneappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
